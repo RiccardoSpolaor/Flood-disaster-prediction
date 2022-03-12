7f69b48a78ee1766fd55d7f80743c200cc5bdf51
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-03-12 18:27:19 +0100

    utils functions implemented

--- utils.py
+++ utils.py
@@ -1,25 +1,51 @@
+import numpy as np
 import pandas as pd
 from io import StringIO
+
+from numpy import ndarray
 from pgmpy.factors.discrete.CPD import TabularCPD
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
-def pick_dictionary_subset(dictionary: Dict, keys: List[str]):
-    return dict((k, dictionary[k]) for k in keys if k in dictionary)
+def get_state_names(variable: str, state_names_dictionary: Dict[str, List[str]],
+                    evidence_dictionary: Dict[str, List[str]]):
+    evidence: Optional[List[str]] = evidence_dictionary[variable]
+    if evidence is None:
+        variable_and_evidence_list = [variable]
+    else:
+        variable_and_evidence_list = [variable] + evidence
+
+    return dict((v, state_names_dictionary[v]) for v in variable_and_evidence_list if v in state_names_dictionary)
 
 
-def pcd_to_pandas(pcd: TabularCPD):
+def cpd_to_pandas(pcd: TabularCPD):
     # String representing the pretty print format of the PCD table
     tabulate_string = str(pcd)
 
-    # Pandas dataframe creation
-    data = (pd.read_csv(StringIO(tabulate_string), sep=r'\|', comment='+', engine='python',
-                        # skiprows=pcd.variable_card,
-                        skipinitialspace=True,
-                        header=[i for i in range(0, len(pcd.state_names) - 1)]).dropna(how='all', axis=1))
+    header: Optional[List[int]] = None if len(pcd.state_names) == 1 else np.arange(0, len(pcd.state_names) - 1).tolist()
 
+    # Pandas dataframe creation
+    data = (pd.read_csv(StringIO(tabulate_string),
+                        sep=r'\|',
+                        comment='+',
+                        engine='python',
+                        skipinitialspace=True,
+                        header=header).dropna(how='all', axis=1)
+            )
+
+    if header is None:
+        data.columns = [data.columns[0], '']
     # The first column is turned into the index of the Dataframe
     data.set_index(data.columns[0], inplace=True, drop=True)
     data.index.name = None
 
     return data
+
+
+def get_evidence_card(variable: str, state_names_dictionary: Dict[str, List[str]],
+                      evidence_dictionary: Dict[str, List[str]]):
+    evidence: Optional[List[str]] = evidence_dictionary[variable]
+    if evidence is None:
+        return None
+    else:
+        return [len(state_names_dictionary[e]) for e in evidence]
