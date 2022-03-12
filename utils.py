2b986cfcc6a2dc6c9d96cc0acd9e5f6e08b3cb74
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-03-12 17:03:59 +0100

    pcd_to_pandas bug fix

--- utils.py
+++ utils.py
@@ -1,26 +1,25 @@
 import pandas as pd
 from io import StringIO
+from pgmpy.factors.discrete.CPD import TabularCPD
+from typing import Dict, List
 
 
-def pick_dictionary_subset(dictionary, keys):
+def pick_dictionary_subset(dictionary: Dict, keys: List[str]):
     return dict((k, dictionary[k]) for k in keys if k in dictionary)
 
 
-def pcd_to_pandas(pcd):
+def pcd_to_pandas(pcd: TabularCPD):
+    # String representing the pretty print format of the PCD table
     tabulate_string = str(pcd)
-    # header = (pd.read_csv(StringIO(tabulate_string), sep=r'\|' , comment='+', engine='python',
-    #                     nrows=pcd.variable_card, header=None)
-    #            .dropna(how='all', axis=1))
+
+    # Pandas dataframe creation
     data = (pd.read_csv(StringIO(tabulate_string), sep=r'\|', comment='+', engine='python',
                         # skiprows=pcd.variable_card,
                         skipinitialspace=True,
-                        header=[i for i in range(0, pcd.variable_card - 1)]).dropna(how='all', axis=1))
+                        header=[i for i in range(0, len(pcd.state_names) - 1)]).dropna(how='all', axis=1))
 
-    # print(header.T[0])
-    # data = data.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-
-    # Replace data's RangeIndex with column labels.
-    # data.columns = data.columns.map(header.T[0].str.strip().to_dict())
+    # The first column is turned into the index of the Dataframe
     data.set_index(data.columns[0], inplace=True, drop=True)
     data.index.name = None
+
     return data
